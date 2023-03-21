1ea6eac76016842f3d6a4d8ec4c4953c13a3b27b
adhishrayas <96647579+adhishrayas@users.noreply.github.com>
2023-03-21 19:48:47 +0530

    removed unrelated files and code

--- api/models.py
+++ api/models.py
@@ -2,41 +2,15 @@ import uuid
 from django.db import models
 
 
-class Alumnimanager(models.Manager):
-    def create_manager(self,name:str,email) -> 'Alumni':
-        if not name:
-            raise ValueError('Alumni must have a name!')
-        if not email:
-            raise ValueError('Alumni must have an email')
-        
-        alumni = self.model()
-        alumni.name = name
-        alumni.email = email
-        alumni.save()
-
-        return alumni
-    
 class Alumni(models.Model):
     name = models.CharField(max_length=75,blank=False)
     email = models.EmailField(blank=False,unique=True)
-    objects = Alumnimanager()
+
 
     def __str__(self):
         return self.name
     
-class Eventsmanager(models.Manager):
-    def create_event(self,eventname:str,date_:str,*args) -> 'Events':
-        if not eventname:
-            raise ValueError('Event must have a name!')
-        if not date_:
-            raise ValueError('Event must have a date!')
-        
-        event = self.model(args)
-        event.eventname = eventname
-        event.date_ = date_
-        event.save()
-        return event
-        
+
 
     
 class Events(models.Model):
@@ -50,7 +24,7 @@ class Events(models.Model):
     date = models.DateField(blank=False)
     text_body = models.TextField(max_length=255,blank=False)
     location = models.TextField(max_length=100,blank=False)
-    objects = Eventsmanager()
+
     
     def __str__(self):
         return self.eventname
--- api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Events
-
-
-class eventsserializer(serializers.ModelSerializer):
-
-    class Meta:
-        fields = ["eventname","eventimage","date","text+"]
